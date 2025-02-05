97aebff7c21d02f126e2987dbc9403542488884b
Daniel Kulakovskiy <d.kul@niuitmo.ru>
2025-02-05 20:19:15 +0300

    try to play with sfml 2d stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/System/Clock.hpp>
+#include <cmath>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "RPG Game (tbd)");
-    window.setFramerateLimit(144);
+int main() {
+  auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "something cool");
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+  sf::Texture bg_texture("test.jpg", false, sf::IntRect({0, 2}, {190, 162}));
+  bg_texture.setRepeated(true);
+  const unsigned width = 190;
+  sf::Sprite bg_sprite(bg_texture);
+  auto width_scaling = 162.f / 190.f;
+  bg_sprite.setTextureRect(sf::IntRect(
+      {0u, 0u}, {static_cast<int>(std::ceil(800.f / width_scaling)), 600u}));
+  bg_sprite.setScale({width_scaling, 1.f});
 
-        window.clear();
-        window.display();
+  sf::Font font("papyrus.ttf");
+  sf::Text text(font);
+  text.setString("game design is my passion");
+  text.setCharacterSize(40);
+  text.setFillColor(sf::Color::Green);
+  text.setOutlineColor(sf::Color::Blue);
+  text.setOutlineThickness(3.f);
+  text.setStyle(sf::Text::Bold);
+  text.setPosition({80.f, 400.f});
+
+  sf::Texture forg_texture("forg.jpg");
+  sf::Sprite forg_sprite(forg_texture);
+  forg_sprite.setScale({0.3f, 0.3f});
+  forg_sprite.setPosition({520.f, 120.f});
+
+  sf::Clock clock;
+  while (window.isOpen()) {
+    sf::Time elapsed = clock.restart();
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
+
+    window.clear();
+    window.draw(bg_sprite);
+    window.draw(text);
+    window.draw(forg_sprite);
+    window.display();
+  }
 }
